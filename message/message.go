50f4c93590b2c765899adb11d1847be0088dcf24
Juliano Martinez <juliano@martinez.io>
2017-09-10 16:48:21 +0200

    changes a bit the logic to avoid race condition. We still receive the message because we read the status from the shell, but it's acceptable for the shell to refresh the data

--- message/message.go
+++ message/message.go
@@ -9,28 +9,34 @@ import (
 
 func worker(host *ssh.Host, input <-chan string) {
 	connection, err := ssh.Connection(host.Hostname)
-	if err == nil {
+	if err != nil {
+		fmt.Printf("Error connection to host %s: %v\n", host.Hostname, err)
+	} else {
 		host.IsConnected = true
 	}
 
 	for command := range input {
-		session, err := ssh.Session(connection)
-		if err != nil {
-			fmt.Printf("Unable to open session: %v\n", err)
-		}
-
-		output, _ := session.CombinedOutput(string(command))
-		for pos, l := range strings.Split(strings.TrimSuffix(string(output), "\n"), "\n") {
-			if pos == 0 {
-				fmt.Printf("\n")
+		if host.IsConnected {
+			session, err := ssh.Session(connection)
+			if err != nil {
+				fmt.Printf("Unable to open session: %v\n", err)
+				host.IsConnected = false
 			}
-			host.Color.Printf("%s: %s\n", host.Hostname, l)
+
+			host.IsWaiting = true
+			output, _ := session.CombinedOutput(string(command))
+			for pos, l := range strings.Split(strings.TrimSuffix(string(output), "\n"), "\n") {
+				if pos == 0 {
+					fmt.Printf("\r")
+				}
+				host.Color.Printf("%s: %s\n", host.Hostname, l)
+			}
+			host.IsWaiting = false
 		}
-		fmt.Printf("pretool>> ")
 	}
 }
 
-func Broker(hosts []*ssh.Host, input <-chan string, status chan<- string) {
+func Broker(hosts []*ssh.Host, input <-chan string) {
 	for _, host := range hosts {
 		host.Channel = make(chan string)
 		go worker(host, host.Channel)
@@ -38,10 +44,7 @@ func Broker(hosts []*ssh.Host, input <-chan string, status chan<- string) {
 
 	for cmd := range input {
 		for _, host := range hosts {
-			if host.IsConnected {
-				host.Channel <- cmd
-			}
+			host.Channel <- cmd
 		}
-		status <- "done"
 	}
 }
