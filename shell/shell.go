dcdf2b051d579a500b7dc6bfc016a0da34c7e622
Juliano Martinez <juliano@martinez.io>
2017-09-10 16:46:04 +0200

    makes the prompt display the number of hosts connected and change the support command to be prefixed by :

--- shell/shell.go
+++ shell/shell.go
@@ -16,11 +16,11 @@ func usage(w io.Writer) {
 }
 
 var completer = readline.NewPrefixCompleter(
-	readline.PcItem("exit"),
-	readline.PcItem("bye"),
-	readline.PcItem("help"),
-	readline.PcItem("status"),
-	readline.PcItem("list"),
+	readline.PcItem(":exit"),
+	readline.PcItem(":bye"),
+	readline.PcItem(":help"),
+	readline.PcItem(":status"),
+	readline.PcItem(":list"),
 )
 
 func filterInput(r rune) (rune, bool) {
@@ -34,11 +34,10 @@ func filterInput(r rune) (rune, bool) {
 
 func Spawn(hosts []*ssh.Host) {
 	command := make(chan string)
-	status := make(chan string)
-	go message.Broker(hosts, command, status)
+	go message.Broker(hosts, command)
 
 	rl, err := readline.NewEx(&readline.Config{
-		Prompt:              "pretool>> ",
+		Prompt:              fmt.Sprintf("pretool(0)/(%d)>> ", len(hosts)),
 		HistoryFile:         "/tmp/readline.tmp",
 		AutoComplete:        completer,
 		InterruptPrompt:     "^C",
@@ -63,31 +62,44 @@ func Spawn(hosts []*ssh.Host) {
 			break
 		}
 
+		connected := 0
+		waiting := 0
+		for _, host := range hosts {
+			if host.IsConnected {
+				connected++
+				if host.IsWaiting {
+					waiting++
+				}
+			}
+		}
+
+		promtp := ""
+		if waiting > 0 {
+			promtp = fmt.Sprintf("pretool(%d)/(%d)>> ", waiting, connected)
+		} else {
+			promtp = fmt.Sprintf("pretool(%d)>> ", connected)
+		}
+		rl.SetPrompt(promtp)
+
 		line = strings.TrimSpace(line)
 		switch {
-		case line == "help":
+		case line == ":help":
 			usage(rl.Stderr())
-		case line == "bye":
+		case line == ":bye":
 			goto exit
-		case line == "exit":
+		case line == ":exit":
 			goto exit
-		case line == "list":
+		case line == ":list":
 			for _, host := range hosts {
 				fmt.Printf("%v: Connected(%v)\n", host.Hostname, host.IsConnected)
 			}
-		case line == "status":
-			count := 0
-			for _, host := range hosts {
-				if host.IsConnected == true {
-					count++
-				}
-			}
-			fmt.Printf("Connected hosts (%d)\n", count)
-			fmt.Printf("Failed hosts (%d)\n", len(hosts)-count)
+		case line == ":status":
+			fmt.Printf("Connected hosts (%d)\n", connected)
+			fmt.Printf("Failed hosts (%d)\n", len(hosts)-connected)
 		case line == "":
 		default:
 			command <- line
-			<-status
+			fmt.Printf(promtp)
 		}
 	}
 exit:
